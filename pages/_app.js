029f74aed91349a9b1d2e49a3a2d4696e742da4f
Mats Andreassen <matsand@live.no>
2020-06-26 13:51:33 +0200

    Standard

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../assets/styles.css'
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps}></Component>
+export default function MyApp ({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
